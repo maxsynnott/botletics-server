e836620bd6f6629fc43348e86b11b69e4e74fd8d
maxsynnott <maxryansynnott@gmail.com>
2021-02-18 00:01:55 +0100

    Initial socket.io implementation

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,32 @@ import { createConnection } from 'typeorm';
 import express from 'express';
 import { router } from './router';
 import { initialize } from './initializer';
+import { createServer } from 'http';
+import { Server as SocketsServer, Socket } from 'socket.io';
 
 createConnection()
 	.then(async () => {
 		const app = express();
+		const httpServer = createServer(app);
+
+		const io = new SocketsServer(httpServer, {
+			cors: {
+				origin: 'http://localhost:3000',
+			},
+		});
+
+		io.on('connection', (socket: Socket) => {
+			console.log('User Connected');
+
+			socket.emit('from-server', 'Hello Client :)');
+		});
 
 		initialize(app);
 
 		app.use(router);
 
-		// start express server
 		const port = process.env.PORT || 8080;
-		app.listen(port, () => {
+		httpServer.listen(port, () => {
 			console.log(`Botletics server listening on port ${port}`);
 		});
 	})
